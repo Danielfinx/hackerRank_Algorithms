4f8e69f6bb02bef4c85a8658b097fc382bf1a729
Daniel <daniel.e.san.1999@gmail.com>
2025-02-08 14:26:42 -0400

    Problem Solved

---
+++ saveThePrisoner/script.js
@@ -0,0 +1,17 @@
+const sampleInput = [
+  [5, 2, 1],
+  [5, 2, 2],
+  [7, 19, 2],
+  [3, 7, 3],
+];
+
+function saveThePrisoner(n, m, s) {
+  let mult = 1;
+  let realPos = m + s - 1;
+  while (n * mult < realPos) {
+    mult++;
+  }
+  return realPos - n * (mult - 1);
+}
+
+sampleInput.forEach((sample) => console.log(saveThePrisoner(...sample)));
