11a676590e5e6914fbac63338a1f8249f2a35586
Daniel <daniel.e.san.1999@gmail.com>
2025-02-16 12:49:17 -0400

    Problem Solved

---
+++ Implementation/stones/script.js
@@ -0,0 +1,23 @@
+const sampleInput = [
+  [3, 1, 2],
+  [4, 10, 100],
+];
+
+function stones(n, a, b) {
+  // Write your code here
+  const outp = [];
+  const actLength = n - 1;
+
+  for (let i = 0; i <= actLength; i++) {
+    const aux = a * i + b * (actLength - i);
+    if (!outp.find((item) => item == aux)) {
+      outp.push(aux);
+    }
+  }
+
+  return outp.sort((a, b) => a - b);
+}
+
+for (let sample of sampleInput) {
+  console.log(stones(...sample));
+}
