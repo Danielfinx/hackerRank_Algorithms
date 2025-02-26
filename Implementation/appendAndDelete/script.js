ac93526ac8c4584a765ffe970759db470e7c2aad
Daniel <daniel.e.san.1999@gmail.com>
2025-02-26 13:44:21 -0400

    Problem Solved

--- Implementation/appendAndDelete/script.js
+++ Implementation/appendAndDelete/script.js
@@ -5,9 +5,33 @@ const sampleInput = [
 ];
 
 function appendAndDelete(s, t, k) {
-  if (s === t) {
+  if (s === t || k >= s.length + t.length) {
     return "Yes";
   }
+
+  let commonLength = 0;
+  for (let i = 0; i < Math.min(s.length, t.length); i++) {
+    if (s[i] === t[i]) {
+      commonLength++;
+    } else {
+      break;
+    }
+  }
+
+  const diff = s.length + t.length - 2 * commonLength;
+  if (diff > k) {
+    return "No";
+  }
+
+  if ((k - diff) % 2 === 0) {
+    return "Yes";
+  }
+
+  if (diff % 2 === 0) {
+    return "Yes";
+  }
+
+  return "No";
 }
 
 for (const sample of sampleInput) {
