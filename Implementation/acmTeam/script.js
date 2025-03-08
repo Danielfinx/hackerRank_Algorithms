bc2cf03883849a3597c6fc2bf165a3311dfd0cdf
Daniel <daniel.e.san.1999@gmail.com>
2025-03-08 14:13:03 -0400

    Initial Commit of the Problem

---
+++ Implementation/acmTeam/script.js
@@ -0,0 +1,10 @@
+const sampleInput = [
+  ["10101", "11110", "00010"],
+  ["10101", "11100", "11010", "00101"],
+];
+
+function acmTeam(topic) {
+  // Write your code here
+}
+
+sampleInput.forEach((sample) => console.log(acmTeam(sample)));
